9b5ca9b35c769d28668b705e763740a733638fd2
Tate Thurston <tatethurston@gmail.com>
2022-07-31 00:10:44 -0700

    add astrojs-service-worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tatethurston.com",
-  integrations: [preact()],
+  integrations: [preact(), serviceWorker()],
   // S3 will redirect non trailing slashes
   trailingSlash: "always",
 });
