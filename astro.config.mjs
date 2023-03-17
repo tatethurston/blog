6ca5568b93099dd7457f8c5b82eb69cb4d70ba6d
Tate Thurston <tatethurston@gmail.com>
2023-03-17 14:44:31 -0700

    updates (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import serviceWorker from "astrojs-service-worker";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.tatethurston.com",
-  integrations: [preact(), serviceWorker()],
+  integrations: [preact(), serviceWorker(), sitemap()],
   trailingSlash: "never",
   build: {
     format: "file",
